f564a98a5dfc97d56491074032c21203402d752e
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-18 00:24:04 -0300

    Name, price and units ready.

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ number_items = int(input("How many items do you want to register?"))
 
 for i in range(number_items):
     list_items.append({})
-    inserted_name = input("Whats is the item's name?")
+    inserted_name = input("Whats is the product name?")
     list_items[i]["name"] = inserted_name
+    inserted_price = input("Whats is the product price?")
+    list_items[i]["price"] = inserted_price
+    inserted_units = input("How many units of the product?")
+    list_items[i]["units"] = inserted_units
 
 print(list_items)
