b73d36791c1367e9acb1c05efc72db9b4c706655
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-18 00:17:36 -0300

    Number of items and name ready.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Create a product registration program.
 # Criar um programa para registro de produtos.
+
+list_items = []
+
+number_items = int(input("How many items do you want to register?"))
+
+for i in range(number_items):
+    list_items.append({})
+    inserted_name = input("Whats is the item's name?")
+    list_items[i]["name"] = inserted_name
+
+print(list_items)
